c6608262d42c8317e2768875827628789903a4fb
ming <wang_ming@iclould.com>
2020-02-13 16:13:58 +0800

    create compute engine instance

--- terraform/lab1/main.tf
+++ terraform/lab1/main.tf
@@ -9,7 +9,7 @@ resource "random_id" "instance_id" {
 }
 
 resource "google_compute_instance" "default" {
-  name = "flask-vm-${random_id.instance_id.hex}"
+  name = "ming-vm-${random_id.instance_id.hex}"
   machine_type = "f1-micro"
   zone = "us-west1-b"
 
